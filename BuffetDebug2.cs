82eb52f157cc1fe8d47fd5f9998ae189dd87da18
Luke <photos.luke.martin@gmail.com>
2022-08-31 21:18:17 -0400

    Add project files.

---
+++ BuffetDebug2.cs
@@ -0,0 +1,5 @@
+﻿string response;
+decimal order_total_price, drink_total_price, buffet_total_price;
+int ask_count = 0;
+int W = 0; //number of waters ordered
+int C = 0; //number of coffee ordered
